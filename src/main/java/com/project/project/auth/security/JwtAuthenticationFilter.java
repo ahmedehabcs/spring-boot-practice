2cae5955ab27e7c16afaa0ed7b9742d180b60f30
Amado Ehab <amadoehab@gmail.com>
2026-09-22 21:26:29 +0300

    applied roles for users

--- src/main/java/com/project/project/auth/security/JwtAuthenticationFilter.java
+++ src/main/java/com/project/project/auth/security/JwtAuthenticationFilter.java
@@ -1,30 +1,29 @@
 package com.project.project.auth.security;
 
 import com.project.project.auth.service.JwtService;
+import com.project.project.common.exception.ApiException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.jspecify.annotations.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    private final JwtService jwtService;
-    private final CustomUserDetailsService userDetailsService;
-
-    public JwtAuthenticationFilter( JwtService jwtService,  CustomUserDetailsService userDetailsService) {
-        this.jwtService = jwtService;
-        this.userDetailsService = userDetailsService;
-    }
+    @Autowired
+    private JwtService jwtService;
 
     @Override
     protected void doFilterInternal(
@@ -43,33 +42,17 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         String token = authHeader.substring(7);
 
         try {
-            String email = jwtService.extractEmail(token);
 
-            if (email != null
-                    && SecurityContextHolder.getContext().getAuthentication() == null
-                    && !jwtService.isTokenExpired(token)) {
-
-                UserDetails userDetails =
-                        userDetailsService.loadUserByUsername(email);
-
-                UsernamePasswordAuthenticationToken authentication =
-                        new UsernamePasswordAuthenticationToken(
-                                userDetails,
-                                null,
-                                userDetails.getAuthorities()
-                        );
-
-                authentication.setDetails(
-                        new WebAuthenticationDetailsSource()
-                                .buildDetails(request)
-                );
-
-                SecurityContextHolder.getContext()
-                        .setAuthentication(authentication);
+            if (SecurityContextHolder.getContext().getAuthentication() == null && !jwtService.isTokenExpired(token)) {
+                String email = jwtService.extractEmail(token);
+                String role = jwtService.extractRole(token);
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(email, null, List.of(new SimpleGrantedAuthority(role)));
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
             }
 
         } catch (Exception ignored) {
-            // invalid/expired JWT -> user stays unauthenticated
+            throw new ApiException(HttpStatus.UNAUTHORIZED, "Unauthorized");
         }
 
         filterChain.doFilter(request, response);
--- src/main/java/com/project/project/user/controller/UserController.java
+++ src/main/java/com/project/project/user/controller/UserController.java
@@ -7,6 +7,7 @@ import com.project.project.user.dto.UpdateUserRequest;
 import com.project.project.user.dto.UserResponse;
 import com.project.project.user.service.UserService;
 import jakarta.validation.Valid;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,31 +22,37 @@ public class UserController {
         this.userService = userService;
     }
 
+    @PreAuthorize("hasAnyAuthority('SUPERADMIN', 'ADMIN', 'EMPLOYEE')")
     @GetMapping
     public List<UserResponse> getAllUsers(){
         return userService.getAllUsers();
     }
 
+    @PreAuthorize("hasAnyAuthority('SUPERADMIN', 'ADMIN', 'EMPLOYEE')")
     @GetMapping("/{id}")
     public UserResponse getUserById(@PathVariable Long id){
         return userService.getUserById(id);
     }
 
+    @PreAuthorize("hasAnyAuthority('SUPERADMIN', 'ADMIN', 'EMPLOYEE')")
     @GetMapping("/by-email")
     public UserResponse getUserByEmail(@RequestParam String email){
         return userService.getUserByEmail(email);
     }
 
+    @PreAuthorize("hasAnyAuthority('SUPERADMIN', 'ADMIN', 'EMPLOYEE')")
     @PostMapping
-    public UserResponse createUser(@Valid @RequestBody CreateUserRequest  createUserRequest){
+    public UserResponse createUser(@Valid @RequestBody CreateUserRequest createUserRequest){
         return userService.createUser(createUserRequest);
     }
 
+    @PreAuthorize("hasAnyAuthority('SUPERADMIN', 'ADMIN')")
     @PatchMapping("/{id}")
-    public UserResponse updateUser(@PathVariable Long id , @Valid @RequestBody UpdateUserRequest updateUserRequest){
-        return  userService.updateUser(id, updateUserRequest);
+    public UserResponse updateUser(@PathVariable Long id, @Valid @RequestBody UpdateUserRequest updateUserRequest){
+        return userService.updateUser(id, updateUserRequest);
     }
 
+    @PreAuthorize("hasAuthority('SUPERADMIN')")
     @DeleteMapping("/{id}")
     public ApiResponse<Void> deleteUser(@PathVariable Long id){
         userService.deleteUser(id);
